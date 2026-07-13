d938559eace7cae77e61f696a101538b3f5c7b78
Govind Gopan <govindgopan2004@gmail.com>
2026-07-13 17:55:33 +0530

    condition and loops

---
+++ Day 6/CW.py
@@ -0,0 +1,29 @@
+attendance = [18, 20, 19, 15, 21]
+x =0
+count = 0
+Total_attendance =0
+for x in attendance :
+    Total_attendance += x
+
+    if x >= 20 :
+        print("Full")
+        count += 1
+    
+    else :
+        print("Not Full")
+ 
+print(f"\nAttendance was full for {count} days")      
+
+print(f"\nTotal attendance for 5 days = {Total_attendance}")
+
+    
+        
+
+   
+
+
+
+    
+
+   
+    
